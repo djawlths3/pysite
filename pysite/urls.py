75025b000e5f17dbb569cc0db526132f5e12610e
djawlths3 <djawlths4@naver.com>
2019-06-21 15:46:42 +0900

    0621

--- pysite/urls.py
+++ pysite/urls.py
@@ -26,6 +26,12 @@ urlpatterns = [
     path('user/joinform', user_view.joinform),
     path('user/join', user_view.join),
     path('user/joinsuccess', user_view.joinsuccess),
+    path('user/login', user_view.login),
+    path('user/loginform', user_view.loginform),
+    path('user/logout', user_view.logout),
+    path('user/updateform', user_view.updateform),
+    path('user/update', user_view.update),
+    path('user/api/checkemail', user_view.checkemail),
 
     path('guestbook/deleteform/<int:id>', guestbook_views.deleteform),
     path('guestbook/', guestbook_views.list),
--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponseRedirect
+from django.forms import model_to_dict
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -19,3 +20,51 @@ def join(request):
 
 def joinsuccess(request):
     return render(request, 'user/joinsuccess.html')
+
+def loginform(request):
+    return render(request, 'user/loginform.html')
+
+def login(request):
+    results = User.objects.filter(email=request.POST['email']).filter(password=request.POST['password'])
+    if len(results) == 0:
+        return HttpResponseRedirect('/user/loginform?result=fail')
+
+    authuser = results[0]
+    request.session['authuser'] = model_to_dict(authuser)
+    return HttpResponseRedirect('/')
+
+def logout(request):
+    del request.session['authuser']
+    return HttpResponseRedirect('/')
+
+def updateform(request):
+    user = User.objects.get(id=request.session['authuser']['id'])
+    data = {
+        'user' : user
+    }
+    return render(request, 'user/updateform.html', data)
+
+
+def update(request):
+    user = User.objects.get(id=request.session['authuser']['id'])
+    user.name = request.POST['name']
+    user.gender = request.POST['gender']
+    if request.POST['password'] is not '':
+        user.password = request.POST['password']
+
+    user.save()
+    request.session['authuser'] = model_to_dict(user)
+    return HttpResponseRedirect('/user/updateform?result=success')
+
+
+def checkemail(request):
+    try:
+        user = User.objects.get(email=request.GET['email'])
+    except Exception as e:
+        user = None
+
+    result = {
+        'result' : 'success',
+        'data': 'not exists' if user is None else 'exit',
+    }
+    return JsonResponse(result)
